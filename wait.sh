0feb439a0e81918d4ac5f7c4b992be6811827623
Artem Chebotko <artemchebotko@gmail.com>
2022-08-25 11:45:30 -0500

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -17,9 +17,6 @@ echo "Choose Astra DB or Cassandra on the left panel to start."
 
 echo ""
 
-#curl -Ls "https://dtsx.io/get-astra-cli" | bash >> ./install.log
 
-#source /home/gitpod/.astra/cli/astra-init.sh
-#unset JAVA_TOOL_OPTIONS
 
       
